5a89826d471e3e769fde30ba82ef1721ea4c4270
Leo Berenger <leoberenger@gmail.com>
2018-01-18 10:59:59 +0100

    Added (test) Toast message to check working listener on Comment Button

--- MoodTracker/app/src/main/java/com/openclassrooms/moodtracker/Controllers/Activities/MainActivity.java
+++ MoodTracker/app/src/main/java/com/openclassrooms/moodtracker/Controllers/Activities/MainActivity.java
@@ -1,13 +1,18 @@
 package com.openclassrooms.moodtracker.Controllers.Activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.openclassrooms.moodtracker.Adapters.PageAdapter;
 import com.openclassrooms.moodtracker.R;
@@ -39,9 +44,8 @@ public class MainActivity extends AppCompatActivity {
         mCommentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Open EditText Window
-            }
-        });
+                Toast.makeText(MainActivity.this, "Ajoutez un commentaire", Toast.LENGTH_SHORT).show();
+        }});
 
         this.configureViewPager();
     }
