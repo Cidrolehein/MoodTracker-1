abed56107f8479af9f873423491f9f76181c2ebe
Leo Berenger <leoberenger@gmail.com>
2018-01-18 16:30:48 +0100

    Modified HistoryActivity : Created generic method to set TextViews

--- MoodTracker/app/src/main/java/com/openclassrooms/moodtracker/Controllers/Activities/HistoryActivity.java
+++ MoodTracker/app/src/main/java/com/openclassrooms/moodtracker/Controllers/Activities/HistoryActivity.java
@@ -17,35 +17,27 @@ public class HistoryActivity extends AppCompatActivity {
     }
 
     private void configureTextViews(){
-        TextView textView7 = (TextView) findViewById(R.id.activity_history_text_view_7);
-        TextView textView6 = (TextView) findViewById(R.id.activity_history_text_view_6);
-        TextView textView5 = (TextView) findViewById(R.id.activity_history_text_view_5);
-        TextView textView4 = (TextView) findViewById(R.id.activity_history_text_view_4);
-        TextView textView3 = (TextView) findViewById(R.id.activity_history_text_view_3);
-        TextView textView2 = (TextView) findViewById(R.id.activity_history_text_view_2);
-        TextView textView1 = (TextView) findViewById(R.id.activity_history_text_view_1);
+        TextView [] textViews = {(TextView) findViewById(R.id.activity_history_text_view_7),
+                                 (TextView) findViewById(R.id.activity_history_text_view_6),
+                                 (TextView) findViewById(R.id.activity_history_text_view_5),
+                                 (TextView) findViewById(R.id.activity_history_text_view_4),
+                                 (TextView) findViewById(R.id.activity_history_text_view_3),
+                                 (TextView) findViewById(R.id.activity_history_text_view_2),
+                                 (TextView) findViewById(R.id.activity_history_text_view_1)};
 
         int [] feelings = {0, 1, 2, 3, 4};
+        int [] viewSize = {100, 250, 400, 550, 700};
+        String [] dayCount = {"Il y a une semaine", "Il y a 6 jours", "Il y a 5 jours","Il y a 4 jours", "Il y a 3 jours", "Avant-hier", "Hier"};
+
         int intFeeling = feelings[3];
 
-        int [] viewSize = {100, 250, 400, 550, 700};
-
-
-        switch(intFeeling){
-            case 0:
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                textView7.getLayoutParams().width = viewSize[intFeeling];
-                textView7.setBackgroundColor(getResources().getIntArray(R.array.colorPagesViewPager)[intFeeling]);
-                break;
-            case 4:
-                break;
-            default:
-                break;
+        for (int i = 0; i < textViews.length; i++){
+            textViews[i].setText(dayCount[i]);
+            textViews[i].setTextSize(20);
+            textViews[i].setPadding(10,0,0,0);
+            textViews[i].setBackgroundColor(getResources().getIntArray(R.array.colorPagesViewPager)[intFeeling]);
+            textViews[i].getLayoutParams().width = viewSize[intFeeling];
         }
     }
+
 }
