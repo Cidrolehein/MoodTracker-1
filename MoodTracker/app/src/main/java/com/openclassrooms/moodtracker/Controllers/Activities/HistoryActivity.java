e34971bffb7a665db665d31ca287c5c646aa09d1
Leo Berenger <leoberenger@gmail.com>
2018-01-18 22:02:14 +0100

    Moved DailyMoods methods to Models/DailyMood

--- MoodTracker/app/src/main/java/com/openclassrooms/moodtracker/Controllers/Activities/HistoryActivity.java
+++ MoodTracker/app/src/main/java/com/openclassrooms/moodtracker/Controllers/Activities/HistoryActivity.java
@@ -1,21 +1,23 @@
 package com.openclassrooms.moodtracker.Controllers.Activities;
 
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.openclassrooms.moodtracker.Models.DailyMood;
 import com.openclassrooms.moodtracker.R;
 
 public class HistoryActivity extends AppCompatActivity {
 
+    private SharedPreferences mPreferences;
+
+    private DailyMood dailyMoods = new DailyMood();
     private int [] viewSize = {250, 500, 750, 1000, 1250};
 
     private String [] dayCount = {"Il y a une semaine", "Il y a 6 jours", "Il y a 5 jours",
             "Il y a 4 jours", "Il y a 3 jours", "Avant-hier", "Hier"};
 
-    private SharedPreferences mPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +26,7 @@ public class HistoryActivity extends AppCompatActivity {
 
         mPreferences = getSharedPreferences("dailyMoods", MODE_PRIVATE);
 
-        updateDailyMoods(mPreferences, 2);
+        dailyMoods.updateDailyMoods(mPreferences, 2);
 
         this.configureTextViews();
     }
@@ -45,7 +47,7 @@ public class HistoryActivity extends AppCompatActivity {
 
     private void setTextView(TextView textView, int i){
 
-        int dailyMood = getDailyMood(mPreferences, i);
+        int dailyMood = dailyMoods.getDailyMood(mPreferences, i);
 
         textView.setText(dayCount[i]);
         textView.setTextSize(20);
@@ -53,35 +55,5 @@ public class HistoryActivity extends AppCompatActivity {
 
         textView.setBackgroundColor(getResources().getIntArray(R.array.colorPagesViewPager)[dailyMood]);
         textView.getLayoutParams().width = viewSize[dailyMood];
-    }
-
-    public int getDailyMood(SharedPreferences prefsFile, int numDay) {
-        int dailyMood = prefsFile.getInt("day"+numDay, 4);
-        return dailyMood;
-    }
-
-    public void setDailyMood(SharedPreferences prefsFile, int numDay, int dailyMood) {
-        prefsFile.edit().putInt("day"+numDay, dailyMood).apply();
-    }
-
-    public void updateDailyMoods(SharedPreferences prefsFile, int yesterdayMood){
-
-        int [] dailyMoods = new int [7];
-
-        //Get Last Saved DailyMoods
-        for(int i = 0; i < dailyMoods.length; i++){
-            dailyMoods[i] = getDailyMood(prefsFile, i);
-        }
-
-        //Move every dailyMoods up one day  (3days ago's Mood -> 4days ago's Mood)
-        for(int i = 6; i > 0; i--){
-            dailyMoods[i] = dailyMoods[i-1];
-        }
-        dailyMoods[0] = yesterdayMood;
-
-        //Save updated dailyMoods
-        for(int i = 0; i < dailyMoods.length; i++) {
-            prefsFile.edit().putInt("day" + i, dailyMoods[i]).apply();
-        }
     }
 }
