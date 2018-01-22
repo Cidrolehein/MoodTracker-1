6dac92fb78da708799884be71826be6923ebb3d7
Leo Berenger <leoberenger@gmail.com>
2018-01-22 09:34:27 +0100

    MainActivity : Deleted PageTransformer effect on vertical viewpager

--- MoodTracker/app/src/main/java/com/openclassrooms/moodtracker/Controllers/Activities/MainActivity.java
+++ MoodTracker/app/src/main/java/com/openclassrooms/moodtracker/Controllers/Activities/MainActivity.java
@@ -39,8 +39,6 @@ public class MainActivity extends AppCompatActivity {
 
     int[] smileys = {R.drawable.smiley_sad, R.drawable.smiley_disappointed, R.drawable.smiley_normal, R.drawable.smiley_happy, R.drawable.smiley_super_happy};
 
-    private static final float MIN_SCALE = 0.75f;
-    private static final float MIN_ALPHA = 0.75f;
 
 
     @Override
@@ -77,43 +75,6 @@ public class MainActivity extends AppCompatActivity {
                 getSupportFragmentManager(),
                 getResources().getIntArray(R.array.colorPagesViewPager), smileys) {});
         viewPager.setCurrentItem(intTodayMood);
-
-        viewPager.setPageTransformer(true, new ViewPager.PageTransformer() {
-            @Override
-            public void transformPage(View view, float position) {
-                int pageWidth = view.getWidth();
-                int pageHeight = view.getHeight();
-
-                if (position < -1) { // [-Infinity,-1)
-                    // This page is way off-screen to the left.
-                    view.setAlpha(0);
-
-                } else if (position <= 1) { // [-1,1]
-                    // Modify the default slide transition to shrink the page as well
-                    float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
-                    float vertMargin = pageHeight * (1 - scaleFactor) / 2;
-                    float horzMargin = pageWidth * (1 - scaleFactor) / 2;
-                    if (position < 0) {
-                        view.setTranslationY(vertMargin - horzMargin / 2);
-                    } else {
-                        view.setTranslationY(-vertMargin + horzMargin / 2);
-                    }
-
-                    // Scale the page down (between MIN_SCALE and 1)
-                    view.setScaleX(scaleFactor);
-                    view.setScaleY(scaleFactor);
-
-                    // Fade the page relative to its size.
-                    view.setAlpha(MIN_ALPHA +
-                            (scaleFactor - MIN_SCALE) /
-                                    (1 - MIN_SCALE) * (1 - MIN_ALPHA));
-
-                } else { // (1,+Infinity]
-                    // This page is way off-screen to the right.
-                    view.setAlpha(0);
-                }
-            }
-        });
     }
 
     private void configureDailyMoodWithDate (SharedPreferences prefsFile, int currentDay, WeeklyMoods wm){
